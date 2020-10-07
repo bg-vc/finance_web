58b54f9d75ea87d28897531c280ff4bfbc69c8da
vincecfl <vincecfl@gmail.com>
2020-10-07 16:31:14 +0800

    add

--- lib/page/pc/vault_pc_page.dart
+++ lib/page/pc/vault_pc_page.dart
@@ -3,8 +3,10 @@ import 'package:finance_web/provider/common_provider.dart';
 import 'package:finance_web/router/application.dart';
 import 'package:finance_web/util/screen_util.dart';
 import 'package:fluro/fluro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 class VaultPcPage extends StatefulWidget {
   @override
@@ -13,6 +15,7 @@ class VaultPcPage extends StatefulWidget {
 
 class _VaultPcPageState extends State<VaultPcPage> {
   var _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool _layoutFlag = false;
 
   @override
   void initState() {
@@ -70,7 +73,7 @@ class _VaultPcPageState extends State<VaultPcPage> {
       child: ListView(
         children: <Widget>[
           SizedBox(height: 50),
-          _oneWidget(context),
+          !_layoutFlag ? _oneWidget(context) : _twoWidget(context),
         ],
       ),
     );
@@ -78,7 +81,11 @@ class _VaultPcPageState extends State<VaultPcPage> {
 
   Widget _oneWidget(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        setState(() {
+          _layoutFlag = !_layoutFlag;
+        });
+      },
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -227,7 +234,9 @@ class _VaultPcPageState extends State<VaultPcPage> {
             child: Card(
               child: InkWell(
                 onTap: () {
-
+                  setState(() {
+                    _layoutFlag = !_layoutFlag;
+                  });
                 },
                 child: Container(
                   width: 50,
@@ -235,8 +244,185 @@ class _VaultPcPageState extends State<VaultPcPage> {
                   color: MyColors.blue500,
                   alignment: Alignment.center,
                   child: Icon(
-                    Icons.add,
-                    size: 26,
+                    CupertinoIcons.down_arrow,
+                    size: 23,
+                    color: MyColors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _twoWidget(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _layoutFlag = !_layoutFlag;
+        });
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 1200,
+            height: 360,
+            child: Card(
+              elevation: 0,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
+              child: _twoBizWidget(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _twoBizWidget(BuildContext context) {
+    return Container(
+      width: 1200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 10),
+            child: ClipOval(
+              child: Image.asset(
+                'images/usdt.png',
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(width: 30),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'USDT',
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      color: MyColors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  child: Text(
+                    'USDT',
+                    style: GoogleFonts.lato(
+                      fontSize: 14,
+                      color: MyColors.grey700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 150),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '66.6126 USDT',
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      color: MyColors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  child: Text(
+                    '余额',
+                    style: GoogleFonts.lato(
+                      fontSize: 14,
+                      color: MyColors.grey700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 220),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '20.5600 USDT',
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      color: MyColors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  child: Text(
+                    '已存入',
+                    style: GoogleFonts.lato(
+                      fontSize: 14,
+                      color: MyColors.grey700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 100),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '11.68%',
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      color: MyColors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  child: Text(
+                    '回报率',
+                    style: GoogleFonts.lato(
+                      fontSize: 14,
+                      color: MyColors.grey700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 100),
+          Container(
+            child: Card(
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    _layoutFlag = !_layoutFlag;
+                  });
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: MyColors.blue500,
+                  alignment: Alignment.center,
+                  child: Icon(
+                    CupertinoIcons.up_arrow,
+                    size: 23,
                     color: MyColors.white,
                   ),
                 ),
@@ -255,13 +441,11 @@ class _VaultPcPageState extends State<VaultPcPage> {
       titleSpacing: 0.0,
       leading: _leadingWidget(context),
       title: Container(
-        margin:
-            EdgeInsets.only(left: LocalScreenUtil.getInstance().setWidth(20)),
+        margin: EdgeInsets.only(left: LocalScreenUtil.getInstance().setWidth(20)),
         child: Row(
           children: [
             Container(
-              child: Image.asset('images/aaa.png',
-                  fit: BoxFit.contain, width: 80, height: 80),
+              child: Image.asset('images/aaa.png', fit: BoxFit.contain, width: 80, height: 80),
             ),
           ],
         ),
@@ -325,11 +509,9 @@ class _VaultPcPageState extends State<VaultPcPage> {
           });
         }
         if (index == 0) {
-          Application.router
-              .navigateTo(context, 'vault', transition: TransitionType.fadeIn);
+          Application.router.navigateTo(context, 'vault', transition: TransitionType.fadeIn);
         } else if (index == 1) {
-          Application.router
-              .navigateTo(context, 'farm', transition: TransitionType.fadeIn);
+          Application.router.navigateTo(context, 'farm', transition: TransitionType.fadeIn);
         }
       },
     );
