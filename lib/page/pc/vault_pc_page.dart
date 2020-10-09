e9a3ef658e271238a358621bd7529dfcbb197704
vincecfl <vincecfl@gmail.com>
2020-10-09 21:16:53 +0800

    add

--- lib/page/pc/vault_pc_page.dart
+++ lib/page/pc/vault_pc_page.dart
@@ -64,8 +64,6 @@ class _VaultPcPageState extends State<VaultPcPage> {
         selection: TextSelection.fromPosition(TextPosition(affinity: TextAffinity.downstream, offset: _depositAmount.length))));
     _withdrawAmountController =  TextEditingController.fromValue(TextEditingValue(text: _withdrawAmount,
         selection: TextSelection.fromPosition(TextPosition(affinity: TextAffinity.downstream, offset: _withdrawAmount.length))));
-    //_depositAmount  = Provider.of<IndexProvider>(context).depositAmount;
-    //_withdrawAmount  = Provider.of<IndexProvider>(context).withdrawAmount;
 
     return Material(
       color: MyColors.bg,
@@ -168,8 +166,6 @@ class _VaultPcPageState extends State<VaultPcPage> {
       onTap: () {
         setState(() {
           _layoutFlag = !_layoutFlag;
-          //Provider.of<IndexProvider>(context, listen: false).changeDepositAmount('');
-          //Provider.of<IndexProvider>(context, listen: false).changeWithdrawAmount('');
           _depositAmount = '';
           _withdrawAmount = '';
         });
@@ -418,12 +414,11 @@ class _VaultPcPageState extends State<VaultPcPage> {
                         inputFormatters: [MyNumberTextInputFormatter(digit:6)],
                         onChanged: (String value) {
                           if (value != null && value != '') {
-                            //Provider.of<IndexProvider>(context, listen: false).changeDepositAmount(value);
                             _depositAmount = value;
                           } else {
-                            //Provider.of<IndexProvider>(context, listen: false).changeDepositAmount('');
                             _depositAmount = '';
                           }
+                          setState(() {});
                         },
                         onSaved: (String value) {},
                         onEditingComplete: () {},
@@ -520,12 +515,11 @@ class _VaultPcPageState extends State<VaultPcPage> {
                         inputFormatters: [MyNumberTextInputFormatter(digit:6)],
                         onChanged: (String value) {
                           if (value != null && value != '') {
-                            //Provider.of<IndexProvider>(context, listen: false).changeWithdrawAmount(value);
                             _withdrawAmount = value;
                           } else {
-                            //Provider.of<IndexProvider>(context, listen: false).changeWithdrawAmount('');
                             _withdrawAmount = '';
                           }
+                          setState(() {});
                         },
                         onSaved: (String value) {},
                         onEditingComplete: () {},
@@ -593,10 +587,8 @@ class _VaultPcPageState extends State<VaultPcPage> {
             double value = NumUtil.multiply(balance, rateDouble);
             setState(() {
               if (type == 1) {
-                //Provider.of<IndexProvider>(context, listen: false).changeDepositAmount(value.toString());
                 _depositAmount = value.toString();
               } else {
-                //Provider.of<IndexProvider>(context, listen: false).changeWithdrawAmount(value.toString());
                 _withdrawAmount = value.toString();
               }
             });
